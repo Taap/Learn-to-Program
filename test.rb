03417e8185c60d3a8c1032260951525f3b47114f
Tatsuya Miyauchi <smileback77@gmail.com>
2012-07-24 11:06:58 +0900

    Learn Programing

---
+++ test.rb
@@ -0,0 +1,9 @@
+#! /usr/bin/ruby
+
+puts 'What is your lastname?'
+last_name = gets.chomp
+puts 'What is your firtsname?'
+first_name = gets.chomp
+full_name = first_name + ' ' + last_name
+puts 'Your name is ' +  full_name
+
