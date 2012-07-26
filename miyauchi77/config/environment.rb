ef09d0f393563f8720cd5f2e05bf4a837b41508f
Tatsuya Miyauchi <smileback77@gmail.com>
2012-07-26 13:44:07 +0900

    test

---
+++ miyauchi77/config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Miyauchi77::Application.initialize!
---
+++ miyauchi77/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miyauchi77::Application.config.secret_token = '1a3ed1756cd9b9311b3b9c3116f4186c11d0d4f9054f123c92b3365c473ff74cacd3129e2828fb53d381d8da0361dba9ea56967d6d951d8324557a801372de0f'
